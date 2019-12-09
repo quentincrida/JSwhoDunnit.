06949dab3e39a899ff81055964b4f268796743c7
Quentin Crida <qcrida@gmail.com>
2019-12-09 21:24:26 +0000

    third Episode

--- whodunnit.js
+++ whodunnit.js
@@ -1,32 +1,48 @@
 //Episode 1
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
-
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
-}
-
-const verdict = declareMurderer();
-console.log(verdict);
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
 
 //Verdict: Miss Scarlet
 
 // //Episode 2
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
-}
+//verdict: Professor Plum, because its a const which cannot be changed.
+
+//Episode 3
+let murderer = 'Professor Plum';
 
 const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
   return `The murderer is ${murderer}.`;
 }
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
 
-//verdict: Professor Plum, because its a const which cannot be changed.
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// I reckon Mrs Peacock both times, because Mr Plum is outside the block
