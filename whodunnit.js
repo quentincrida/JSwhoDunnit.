8d7de6142bed90a8fce70bf1126c6f0754b72a5f
Quentin Crida <qcrida@gmail.com>
2019-12-09 21:30:42 +0000

    Episode 4

--- whodunnit.js
+++ whodunnit.js
@@ -32,17 +32,33 @@
 //verdict: Professor Plum, because its a const which cannot be changed.
 
 //Episode 3
-let murderer = 'Professor Plum';
-
-const declareMurderer = function() {
-  let murderer = 'Mrs. Peacock';
-  return `The murderer is ${murderer}.`;
-}
-
-const firstVerdict = declareMurderer();
-console.log('First Verdict: ', firstVerdict);
-
-const secondVerdict = `The murderer is ${murderer}.`;
-console.log('Second Verdict: ', secondVerdict);
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
 
 // I reckon Mrs Peacock both times, because Mr Plum is outside the block
+
+//Episode 4
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+//Suspect 3 is Mrs Peacock, because Colonel Mustard is only in the for loop block.
