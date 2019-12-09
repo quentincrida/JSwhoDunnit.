dbf810161bb2506dcf38e88a7b3e76f8bd0ecd02
Quentin Crida <qcrida@gmail.com>
2019-12-09 16:43:47 +0000

    Episode 1

---
+++ whodunnit.js
@@ -0,0 +1,15 @@
+//Episode 1
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+//Verdict: Miss Scarlet
