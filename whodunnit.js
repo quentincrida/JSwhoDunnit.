fd0ad67245cbbd0b553ce314e8556198aac54ba1
Quentin Crida <qcrida@gmail.com>
2019-12-09 17:44:56 +0000

    Episode 2

--- whodunnit.js
+++ whodunnit.js
@@ -13,3 +13,20 @@ const verdict = declareMurderer();
 console.log(verdict);
 
 //Verdict: Miss Scarlet
+
+// //Episode 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//verdict: Professor Plum, because its a const which cannot be changed.
